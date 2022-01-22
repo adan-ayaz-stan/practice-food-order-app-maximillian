3a0eefebab67c809547e7c72c7fa0d8ac0ca6ac8
adan-ayaz-stan <999a.d.a.n.a.y.a.z999@gmail.com>
2022-01-22 14:52:49 +0500

    Third Commit - VS Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
-import { Fragment } from "react/cjs/react.production.min";
+import { Fragment, useState } from "react/cjs/react.production.min";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,23 @@
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = () => {};
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          steps: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
